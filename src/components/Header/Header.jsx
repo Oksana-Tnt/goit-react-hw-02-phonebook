0e5d880c5ee57fc7884a2abd5e3bf55630d955f7
Oksana-Tnt <oksanatantsiura@gmail.com>
2023-09-16 18:33:16 +0200

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,9 +2,9 @@ const Header = ({ showModal }) => {
   return (
     <nav className="navbar bg-dark mb-3">
       <div className="container-fluid">
-        <span className="navbar-brand mb-0 h1 text-success"> Navbar</span>
+        <span className="navbar-brand mb-0 h1 text-success"> Phonebook</span>
         <button className="btn btn-outline-success" onClick={showModal}>
-          Open Modal
+          Add Contact
         </button>
       </div>
     </nav>
