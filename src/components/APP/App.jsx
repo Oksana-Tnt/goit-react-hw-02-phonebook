a1a484d8e9bb0ccb9ba2b847b2c5872b9c347177
Oksana-Tnt <oksanatantsiura@gmail.com>
2023-09-16 16:42:22 +0200

    update task2

---
+++ src/components/APP/App.jsx
@@ -0,0 +1,76 @@
+import FormContact from 'components/FormContact/FormContact';
+import { Container } from './APP.styled';
+import Modal from '../Modal/Modal';
+import { Component } from 'react';
+import Header from 'components/Header/Header';
+import { nanoid } from 'nanoid';
+import ContactList from 'components/ContactList/ContactList';
+import Filter from 'components/Filter/Filter';
+
+export class App extends Component {
+  state = {
+    isShowModal: false,
+    contacts: [],
+    filter: '',
+  };
+
+  addContact = ({ name, number }) => {
+    const newContact = {
+      id: nanoid(),
+      name,
+      number,
+    };
+    this.setState(({ contacts }) => ({
+      contacts: [newContact, ...contacts],
+    }));
+  };
+
+  deleteContact = id => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== id),
+    }));
+  };
+
+  onChangeFilter = e => {
+    this.setState({ filter: e.currentTarget.value });
+  };
+
+  getFilteredContacts = () => {
+    const { contacts, filter } = this.state;
+    const normalizedFilter = filter.toLowerCase();
+    return contacts.filter(contact =>
+      contact.name.toLowerCase().includes(normalizedFilter)
+    );
+  };
+  showModal = () => {
+    this.setState({ isShowModal: true });
+  };
+
+  closeModal = () => {
+    this.setState({ isShowModal: false });
+  };
+
+  render() {
+    const { contacts, filter } = this.state;
+    const filteredContacts = this.getFilteredContacts();
+
+    return (
+      <Container>
+        <Header showModal={this.showModal} />
+        <Filter value={filter} onChange={this.onChangeFilter} />
+        {this.state.isShowModal && (
+          <Modal closeModal={this.closeModal}>
+            <FormContact
+              closeModal={this.closeModal}
+              onSubmit={this.addContact}
+            />
+          </Modal>
+        )}
+        <ContactList
+          contacts={filteredContacts}
+          onDeleteContact={this.deleteContact}
+        />
+      </Container>
+    );
+  }
+}
---
+++ src/components/FormContact/FormContact.jsx
@@ -0,0 +1,67 @@
+import { Component } from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { getValue } from '@testing-library/user-event/dist/utils';
+
+class FormContact extends Component {
+  state = {
+    name: '',
+    number: '',
+  };
+
+  handleChange = ({ target }) => {
+    this.setState({
+      [target.name]: target.value,
+    });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+    this.props.onSubmit({
+      name: this.state.name,
+      number: this.state.number,
+    });
+
+    this.setState({ name: '', number: '' });
+    this.props.closeModal();
+  };
+
+  render() {
+    return (
+      <form className="row g-3" onSubmit={this.handleSubmit}>
+        <div className="mb-3">
+          <label htmlFor="exampleFormControlInput1" className="form-label">
+            Name
+          </label>
+          <input
+            name="name"
+            type="text"
+            className="form-control"
+            id="exampleFormControlInput1"
+            placeholder="name"
+            onChange={this.handleChange}
+            value={this.state.name}
+          />
+          <label htmlFor="exampleFormControlInput1" className="form-label">
+            Number
+          </label>
+          <input
+            name="number"
+            type="tel"
+            className="form-control"
+            id="exampleFormControlInput1"
+            placeholder="number"
+            onChange={this.handleChange}
+            value={this.state.number}
+          />
+        </div>
+        <div className="mb-3">
+          <button type="submit" className="btn btn-primary mb-3">
+            Add contact
+          </button>
+        </div>
+      </form>
+    );
+  }
+}
+
+export default FormContact;
